0b9d22d1b292f497b8a1514fb2864c4c9142f8e0
AnandCodeFc <anandcoder54@gmail.com>
2024-10-24 21:33:33 +0530

    Update erc20.sol

--- erc20.sol
+++ erc20.sol
@@ -1,1 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
 
+// Import OpenZeppelin contracts
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    
+    // The constructor initializes the token with its name and symbol.
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+
+    /**
+     * @dev Mints new tokens.
+     * @param to The address that will receive the minted tokens.
+     * @param amount The number of tokens to mint.
+     */
+    function mint(address to, uint256 amount) public  {
+        _mint(to, amount);
+    }
+
+    /**
+     * @dev Burns tokens from the caller's account.
+     * @param amount The number of tokens to burn.
+     */
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
+    /**
+     * @dev Transfers tokens to a specified address.
+     * @param to The address to transfer to.
+     * @param amount The amount of tokens to be transferred.
+     * @return A boolean value indicating whether the operation succeeded.
+     */
+    function transfer(address to, uint256 amount) public override returns (bool) {
+        return super.transfer(to, amount);
+    }
+
+    /**
+     * @dev Approves the passed address to spend the specified amount of tokens on behalf of the caller.
+     * @param spender The address that will spend the tokens.
+     * @param amount The amount of tokens to be spent.
+     * @return A boolean value indicating whether the operation succeeded.
+     */
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        return super.approve(spender, amount);
+    }
+}
